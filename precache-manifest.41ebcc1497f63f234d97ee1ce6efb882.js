91dfc09049a7f092b78e97dff6b852079fb1eed5
Ujwalsingh595 <ujwalsingh595@gmail.com>
2020-07-12 11:46:46 +0530

    Updates

---
+++ precache-manifest.41ebcc1497f63f234d97ee1ce6efb882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d1389c855763b04da4c605bb6a5854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "926163d8776573bba75b",
+    "url": "/static/css/main.083b62a3.chunk.css"
+  },
+  {
+    "revision": "286191faf4e383856078",
+    "url": "/static/js/2.0fa6728a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fa6728a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926163d8776573bba75b",
+    "url": "/static/js/main.c79be600.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
